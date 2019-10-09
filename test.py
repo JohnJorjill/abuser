8439b447b3d235376148d036aae61b08cbff6a53
john jorjill <orgil9506@gmail.com>
2019-10-09 13:15:56 +0800

    Your commit message 6

--- test.py
+++ test.py
@@ -2,13 +2,24 @@ import os
 from random import randrange
 import time 
 
+day_counter = 0
+write_counter = 0
+
 with open('./abuser.py', mode='w') as my_file:
-	for i in range(30):
+	for i in range(5):
+		day_counter = day_counter + 1
+		print(f'writing {i} day')
 		rand_num = randrange(10)
 		for i in range(rand_num):
-			rand_num = randrange(10)
+			write_counter = write_counter + 1
+			print(f'writing {write_counter} times')
+			rand_num2 = randrange(10)
+			my_file = open('./abuser.py', 'w')
 			my_file.write(str(rand_num))
+			print(f'wrote {rand_num2} to file')
+			my_file.close()
 			time.sleep(5)
-			os.system("git add .")
-			os.system(f'git commit --date="{rand_num} day ago" -m "Your commit message"')
-			os.system("git push")
+
+			#os.system("git add .")
+			#os.system(f'git commit --date="{rand_num2} day ago" -m "Your commit message"')
+			#os.system("git push")
