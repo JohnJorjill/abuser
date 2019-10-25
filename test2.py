e1e48583d7b970695b010706a90573056c7706cc
john jorjill <orgil9506@gmail.com>
2019-10-25 13:18:23 +0800

    Your commit message 3

--- test2.py
+++ test2.py
@@ -1,13 +1,15 @@
 from random import randrange
 import os
 
-rand_num = randrange(10)
 rand_day = randrange(50,100)
-my_file = open('./abuser.py', 'a')
-my_file.write(str(rand_num))
-my_file.close()
-os.system("git status")
-os.system("git add .")
-os.system(f'git commit --date="{rand_day} day ago" -m "Your commit message {rand_num}"')
-os.system("git push")
-os.system("git status")
+
+for i in range(5):
+	rand_num = randrange(10)
+	my_file = open('./abuser.py', 'a')
+	my_file.write(str(rand_num))
+	my_file.close()
+	os.system("git status")
+	os.system("git add .")
+	os.system(f'git commit --date="{rand_day} day ago" -m "Your commit message {rand_num}"')
+	os.system("git push")
+	os.system("git status")
